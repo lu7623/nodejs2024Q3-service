f0affc56d8ebb1409f13e395f5d11568bb9d3e34
Lyubov Agulova <lu7623@gmail.com>
2024-11-09 16:40:40 +0300

    feat: handle user getById, delete, update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,85 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
-import { UserService } from './user.service'
-import { CreateUserDto } from './dto/createUser.dto'
-import { UserDto } from './dto/user.dto'
-
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  HttpException,
+  Header,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserDto } from './dto/user.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { Messages } from 'src/utils/messages';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
+  @Header('content-type', 'application/json')
+  @HttpCode(HttpStatus.CREATED)
   async create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto)
+    if (!createUserDto.login || !createUserDto.password) {
+      throw new HttpException(Messages.IncorrectData, HttpStatus.BAD_REQUEST);
+    }
+    const res = this.userService.create(createUserDto);
+    return res.data;
   }
 
   @Get()
-  async getAllUsers(): Promise<UserDto[]> {
-    return this.userService.getAllUsers()
+  @Header('content-type', 'application/json')
+  async getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  @Header('content-type', 'application/json')
+  getPostById(@Param('id') id: string) {
+    const res = this.userService.getUserById(id);
+    if (res?.message === Messages.WrongIdType) {
+      throw new HttpException(Messages.WrongIdType, HttpStatus.BAD_REQUEST);
+    }
+    if (res?.message === Messages.NotFound) {
+      throw new HttpException(Messages.NotFound, HttpStatus.NOT_FOUND);
+    }
+    return res.data;
+  }
+
+  @Put(':id')
+  @Header('content-type', 'application/json')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdateUserDto) {
+    if (!updatePostDto.newPassword || !updatePostDto.oldPassword) {
+      throw new HttpException(Messages.IncorrectData, HttpStatus.BAD_REQUEST);
+    }
+    const res = this.userService.update(id, updatePostDto);
+    if (res?.message === Messages.WrongOldPassword) {
+      throw new HttpException(Messages.WrongOldPassword, HttpStatus.FORBIDDEN);
+    }
+    if (res?.message === Messages.IncorrectData) {
+      throw new HttpException(Messages.IncorrectData, HttpStatus.BAD_REQUEST);
+    }
+    if (res?.message === Messages.NotFound) {
+      throw new HttpException(Messages.NotFound, HttpStatus.NOT_FOUND);
+    }
+    return res.data;
+  }
+
+  @Delete(':id')
+  @Header('content-type', 'application/json')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    const res = this.userService.remove(id);
+    if (res?.message === Messages.WrongIdType) {
+      throw new HttpException(Messages.WrongIdType, HttpStatus.BAD_REQUEST);
+    }
+    if (res?.message === Messages.NotFound) {
+      throw new HttpException(Messages.NotFound, HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,64 @@
-import { Injectable } from '@nestjs/common'
-import { UserDto } from './dto/user.dto'
-import { CreateUserDto } from './dto/createUser.dto'
-
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { ServiceResponse, serviceResponse } from 'src/utils/types';
+import { validate as uuidValidate } from 'uuid';
+import { Messages } from 'src/utils/messages';
+import { userWithoutPassword } from 'src/utils/userWithoutPassword';
 
 @Injectable()
 export class UserService {
-  private readonly users: UserDto[] = []
+  private readonly users: UserDto[] = [];
 
   create(user: CreateUserDto) {
-    let newUser = new UserDto(user.login, user.password)
+    let newUser = new UserDto(user.login, user.password);
     this.users.push(newUser);
-    return newUser;
+    return serviceResponse({ error: false, data: userWithoutPassword(newUser) });
   }
 
   getAllUsers(): UserDto[] {
-    return this.users
+    return this.users;
+  }
+
+  getUserById(id: string): ServiceResponse {
+    if (!uuidValidate(id)) {
+      return serviceResponse({ error: true, message: Messages.WrongIdType });
+    }
+    let user = this.users.find((user) => user.id === id);
+    if (!user) {
+      return serviceResponse({ error: true, message: Messages.NotFound });
+    }
+    return serviceResponse({ error: false, data: user });
+  }
+
+  update(id: string, dto: UpdateUserDto) {
+    let user = this.users.find((user) => user.id === id);
+    if (!uuidValidate(id)) {
+      return serviceResponse({ error: true, message: Messages.WrongIdType });
+    }
+    if (!user) {
+      return serviceResponse({ error: true, message: Messages.NotFound });
+    }
+    if (user.password !== dto.oldPassword) {
+      return serviceResponse({
+        error: true,
+        message: Messages.WrongOldPassword,
+      });
+    }
+    user.password = dto.newPassword;
+    return serviceResponse({ error: false, data: userWithoutPassword(user) });
+  }
+
+  remove(id: string) {
+    if (!uuidValidate(id)) {
+      return serviceResponse({ error: true, message: Messages.WrongIdType });
+    }
+    let userInd = this.users.findIndex((user) => user.id === id);
+    if (userInd !== -1) {
+      this.users.splice(userInd, 1);
+      return serviceResponse({ error: false });
+    }
+    return serviceResponse({ message: Messages.NotFound, error: true });
   }
 }
